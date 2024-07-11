6bcd08cc3a97f19377d182d9048f787ecc37d989
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2024-07-11 00:02:00 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/app-lanchonete/'
+  base: '/app-lanchonete/',
+  define: {
+    'process.env.REACT_APP_API_URL': JSON.stringify('https://api-lanchonete.vercel.app/'),
+  }
 })
